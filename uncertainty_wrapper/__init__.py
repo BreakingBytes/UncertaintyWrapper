fix nargs and nf switched in jacobian
* lucky tests were all symmetrical 
* also fix tile by nobs
* start to setup for threading test
* add complex test using IV curve equations

uncertainty_wrapper/__init__.py
@@ -12,6 +12,10 @@ Diagonals of :math:`dF_{ij}` are standard deviations squared.
 from functools import wraps
 import numpy as np
 import logging
+from threading import Thread
+from Queue import Queue
+
+QUEUE = Queue()
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -23,6 +27,14 @@ __AUTHOR__ = u"Mark Mikofski"
 __EMAIL__ = u'mark.mikofski@sunpowercorp.com'
 
 DELTA = np.finfo(float).eps ** (1.0 / 3.0) / 2.0
+
+
+def partial_derivative(f, x, n, nargs, nobs, delta=DELTA):
+    dx = np.zeros((nargs, nobs))
+    dx[n] += x[n] * DELTA
+    df = (f(x + dx) - f(x - dx)) / dx[n] / 2.0
+    return df
+    #QUEUE.put((n, df))
 
 
 # TODO: make this a class, add DELTA as class variable and flatten as method
@@ -49,14 +61,25 @@ def jacobian(func, x, *args, **kwargs):
     nobs = x.size / nargs  # number of observations
     f = lambda x_: func(x_, *args, **kwargs)
     j = None  # matrix of zeros
+#     for n in xrange(nargs):
+#         thread = Thread(target=partial_derivative, args=(f, x, n, nargs, nobs))
+#         thread.daemon = True
+#         thread.start()
+#     for _ in xrange(nargs):
+#         # derivatives df/d_n
+#         n, df = QUEUE.get()
+#         if j is None:
+#             j = np.zeros((nargs, df.shape[0], nobs))
+#             # better to transpose J once than to transpose df each time
+#             # j[:,:,n] = df.T
+#         j[n] = df
     for n in xrange(nargs):
-        dx = np.zeros((nargs, nobs))
-        dx[n] += x[n] * DELTA
-        df = (f(x + dx) - f(x - dx)) / dx[n] / 2.0
-        # derivatives df/d_n
+        df = partial_derivative(f, x, n, nargs, nobs)
         if j is None:
-            j = np.zeros((df.shape[0], nargs, nobs))
+            j = np.zeros((nargs, df.shape[0], nobs))
         j[n] = df
+        # better to transpose J once than to transpose df each time
+        # j[:,:,n] = df.T
     return j.T
 
 
@@ -64,12 +87,12 @@ def jflatten(j):
     """
     flatten jacobian into 2-D
     """
-    nobs, nf, nx = j.shape
-    nrows, ncols = nf * nobs, nx * nobs
+    nobs, nf, nargs = j.shape
+    nrows, ncols = nf * nobs, nargs * nobs
     jflat = np.zeros((nrows, ncols))
     for n in xrange(nobs):
-        r, c = n * nf, n * nx
-        jflat[r:(r + nf), c:(c + nx)] = j[n]
+        r, c = n * nf, n * nargs
+        jflat[r:(r + nf), c:(c + nargs)] = j[n]
     return jflat
 
 
@@ -81,11 +104,12 @@ def jflatten(j):
 # observations, is it necessary to flatten J first??
 def unc_wrapper(f):
     @wraps(f)
-    def wrapper(x, __covariance__=None, *args, **kwargs):
+    def wrapper(x, __covariance__, *args, **kwargs):
         avg = f(x, *args, **kwargs)
-        if __covariance__ is None:
-            return avg
-        jac = jflatten(jacobian(f, x, *args, **kwargs))
-        cov = np.dot(np.dot(jac, __covariance__), jac.T)
-        return avg, cov
+        jac = jacobian(f, x, *args, **kwargs)
+        nobs = jac.shape[0]
+        cov = np.tile(__covariance__, (nobs, nobs))
+        jac = jflatten(jac)
+        cov = np.dot(np.dot(jac, cov), jac.T)
+        return avg, cov, jac
     return wrapper

uncertainty_wrapper/tests/__init__.py
@@ -4,6 +4,7 @@ from nose.tools import ok_, eq_, raises
 import numpy as np
 from uncertainty_wrapper import unc_wrapper
 import logging
+from scipy.constants import Boltzmann as KB, elementary_charge as QE
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
@@ -19,12 +20,81 @@ def test_unc_wrapper():
     def f(y):
         return np.exp(y)
     
-    avg, var = f(x, __covariance__=cov)
+    avg, var, jac = f(x, __covariance__=cov)
     LOGGER.debug("average = %g", avg)
     LOGGER.debug("variance = %g", var)
     ok_(np.isclose(avg, np.exp(x)))
     ok_(np.isclose(var, cov * np.exp(x) ** 2))
-    return avg, var
+    ok_(np.isclose(jac, np.exp(x)))
+    return avg, var, jac
+
+
+def IV(x, Vd, E0=1000, T0=298.15, kB=KB, qe=QE):
+    Ee, Tc, Rs, Rsh, Isat1_0, Isat2, Isc0, alpha_Isc, Eg = x
+    Vt = Tc * kB / qe
+    Isc = Ee * Isc0 * (1.0 + (Tc - T0) * alpha_Isc)
+    Isat1 = (
+        Isat1_0 * (Tc ** 3.0 / T0 ** 3.0) *
+        np.exp(Eg * qe / kB * (1.0 / T0 - 1.0 / Tc))
+    )
+    Vd_sc = Isc * Rs  # at short circuit Vc = 0 
+    Id1_sc = Isat1 * (np.exp(Vd_sc / Vt) - 1.0)
+    Id2_sc = Isat2 * (np.exp(Vd_sc / 2.0 / Vt) - 1.0)
+    Ish_sc = Vd_sc / Rsh
+    Iph = Isc + Id1_sc + Id2_sc + Ish_sc
+    Id1 = Isat1 * (np.exp(Vd / Vt) - 1.0)
+    Id2 = Isat2 * (np.exp(Vd / 2.0 / Vt) - 1.0)
+    Ish = Vd / Rsh
+    Ic = Iph - Id1 - Id2 - Ish
+    Vc = Vd - Ic * Rs
+    return np.array([Ic, Vc, Ic * Vc])
+
+
+def Voc(x, E0=1000, T0=298.15, kB=KB, qe=QE):
+    Ee, Tc, Rs, Rsh, Isat1_0, Isat2, Isc0, alpha_Isc, Eg = x
+    msg = ['Ee=%g[suns]','Tc=%g[K]','Rs=%g[ohms]','Rsh=%g[ohms]',
+           'Isat1_0=%g[A]','Isat2=%g[A]','Isc0=%g[A]','alpha_Isc=%g[]',
+           'Eg=%g[eV]']
+    LOGGER.debug('\n' + '\n'.join(msg) + '\n', *x)
+    Vt = Tc * kB / qe
+    LOGGER.debug('Vt=%g[V]', Vt)
+    Isc = Ee * Isc0 * (1.0 + (Tc - T0) * alpha_Isc)
+    LOGGER.debug('Isc=%g[A]', Isc)
+    Isat1 = (
+        Isat1_0 * (Tc ** 3.0 / T0 ** 3.0) *
+        np.exp(Eg * qe / kB * (1.0 / T0 - 1.0 / Tc))
+    )
+    LOGGER.debug('Isat1=%g[A]', Isat1)
+    Vd_sc = Isc * Rs  # at short circuit Vc = 0 
+    Id1_sc = Isat1 * (np.exp(Vd_sc / Vt) - 1.0)
+    Id2_sc = Isat2 * (np.exp(Vd_sc / 2.0 / Vt) - 1.0)
+    Ish_sc = Vd_sc / Rsh
+    Iph = Isc + Id1_sc + Id2_sc + Ish_sc
+    # estimate Voc
+    delta = Isat2 ** 2.0 + 4.0 * Isat1 * (Iph + Isat1 + Isat2)
+    return Vt * np.log(((-Isat2 + np.sqrt(delta)) / 2.0 / Isat1) ** 2.0)
+
+
+RS = 0.004267236774264931  # [ohm] series resistance
+RSH = 10.01226369025448  # [ohm] shunt resistance
+ISAT1_0 = 2.286188161253440E-11  # [A] diode one saturation current
+ISAT2 = 1.117455042372326E-6  # [A] diode two saturation current
+ISC0 = 6.3056  # [A] reference short circuit current
+EE = 0.8
+TC = 323.15
+EG = 1.1
+ALPHA_ISC = 0.0003551
+VOC = Voc((EE, TC, RS, RSH, ISAT1_0, ISAT2, ISC0, ALPHA_ISC, EG))
+assert np.isclose(VOC, 0.62816490891656673)
+LOGGER.debug('Voc = %g[V]', VOC)
+VD = np.arange(0, VOC, 0.1)
+X = [[arg] * VD.size for arg in (EE, TC, RS, RSH, ISAT1_0, ISAT2, ISC0, ALPHA_ISC, EG)]
+COV = np.diag(np.random.rand(9))
+
+
+def test_threading():
+    f = unc_wrapper(IV)
+    return f(np.array(X), COV, VD)
 
 
 if __name__ == '__main__':