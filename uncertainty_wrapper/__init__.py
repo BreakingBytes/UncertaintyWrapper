fix small x wasn't scaling delta
* use np.where to select x*delta or just delta if x == 0
* use predetermined uncertainty instead of random

uncertainty_wrapper/__init__.py
@@ -42,7 +42,7 @@ def partial_derivative(f, x, n, nargs, nobs, delta=DELTA):
     """
     dx = np.zeros((nargs, nobs))
     # scale delta by (|x| + 1.0) to avoid noise from machine precision
-    dx[n] += (1.0 + np.abs(x[n])) * delta
+    dx[n] += np.where(x[n], x[n] * delta, delta)
     # apply central difference approximation
     return (f(x + dx) - f(x - dx)) / dx[n] / 2.0
 

uncertainty_wrapper/tests/__init__.py
@@ -97,7 +97,7 @@ assert np.isclose(VOC, 0.62816490891656673)
 LOGGER.debug('Voc = %g[V]', VOC)
 VD = np.arange(0, VOC, 0.005)
 X = np.array([EE, TC, RS, RSH, ISAT1_0, ISAT2, ISC0, ALPHA_ISC, EG])
-COV = np.diag(np.random.rand(X.size) / 1000.0)
+COV = np.diag([1e-6] * X.size)
 X = X.reshape(-1, 1).repeat(VD.size, axis=1)
 COV = np.tile(COV, (VD.size, 1, 1))
 