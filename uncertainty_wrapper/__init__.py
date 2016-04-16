Fixes #1 now units wrapping works with Pint-0.7.2
* units wrapping only works with unc_wrapper_args
* put unc_wrapper_args first (closest to original function), and
  ureg.wraps last (furthest from original function)
* units wrapping only works with positional covariance keys, ie indices

uncertainty_wrapper
-------------------
* remove inspect, since ureg.wraps (and most decorators) do not preserve
  argspec, they typically only see *args and **kwargs
* dumb down specification of covariance keys:
  - use indices for positional args
  - use keywords for keyword args
  - beware though, Pint's ureg.wraps makes everything a positional arg
    so don't use keywords
  - no defaults allowed as covariance keys
* check cov_keys for None first before checking if len(cov_keys) > 0
  because len(None) is a TypeError
* add args_dict, empty if no kwargs, but a dictionary of args and their
  indices that weren't grouped into x so that we can recreate args_ in
  f_
* don't pop kwargs that go into args into dev/null, save them in
  args_dict so they can be used to recreate the correct order of args_
  in f_
* use args_dict in f_, but rename it as args_dict_, we don't really need
  this, but I think it's good practice to make globals locals
* also save the old args_ as args_list, even though we never use them
* recycle the same code from above
TODO: create a function for this since used 2x
* if grouping x and f in wrapper, then unpack avg value before returning

tests
-----
* import pint to test units wrapping, works! yay!
* add ureg.wrapps with return and arg units in decorator, use None for
  cov, jac and datetime, ignore __covariance__
* add units to lat, lon, press, tamb and seconds

uncertainty_wrapper/__init__.py
@@ -13,7 +13,6 @@ SunPower Corp. (c) 2016
 
 from functools import wraps
 import numpy as np
-import inspect
 import logging
 
 logging.basicConfig()
@@ -130,33 +129,40 @@ def unc_wrapper_args(*covariance_keys):
             kwargs.update({k: v for k, v in enumerate(args)})
             args = ()  # empty args
             # group covariance keys
-            if len(cov_keys) > 0:
-                # uses specified keys
-                x = np.array([np.atleast_1d(kwargs.pop(k)) for k in cov_keys])
-            elif cov_keys is None:
+            if cov_keys is None:
                 # use all keys
                 cov_keys = kwargs.keys()
                 x = np.reshape(kwargs.values(), (len(cov_keys), -1))
                 kwargs = {}  # empty kwargs
+            elif len(cov_keys) > 0:
+                # uses specified keys
+                x = np.array([np.atleast_1d(kwargs.pop(k)) for k in cov_keys])
             else:
                 # arguments already grouped
                 x = kwargs.pop(0)  # use first argument
             # remaining args
+            args_dict = {}
             if kwargs:
                 args = [(n, v) for n, v in kwargs.iteritems()
                         if not isinstance(n, basestring)]
                 # sort by index
                 idx, args = zip(*sorted(args, key=lambda m: m[0]))
-                for n in idx: kwargs.pop(n)  # remove args from kwargs
+                # remove args from kwargs
+                args_dict = {n: kwargs.pop(n) for n in idx}
 
             def f_(x_, *args_, **kwargs_):
+                args_dict_ = args_dict
+                args_list = list(args_)
                 if cov_keys:
-                    kwargs_.update(zip(cov_keys, x_))
-                    args_list = reversed(list(args_))
-                    args_ = [(n, kwargs_.pop(n)) for n in cov_keys
-                             if not isinstance(n, basestring)]
-                    args_ = [a if n in cov_keys else args_list.pop() for n, a
-                             in sorted(args_, key=lambda m: m[0])]
+                    kwargs_.update(zip(cov_keys, x_), **args_dict_)
+                    LOGGER.debug(kwargs_)
+                if kwargs_:
+                    args_ = [(n, v) for n, v in kwargs_.iteritems()
+                            if not isinstance(n, basestring)]
+                    # sort by index
+                    idx, args_ = zip(*sorted(args_, key=lambda m: m[0]))
+                    # remove args_ from kwargs_
+                    args_dict_ = {k: kwargs_.pop(n) for n in idx}
                     return np.array(f(*args_, **kwargs_))
                 # assumes independent and dependent vars already grouped
                 return f(x_, *args_, **kwargs_)
@@ -172,6 +178,8 @@ def unc_wrapper_args(*covariance_keys):
             if cov is not None:
                 cov *= x.T.flatten() ** 2
                 cov = np.dot(np.dot(jac, cov), jac.T)
+            if cov_keys is None or len(cov_keys) > 0:
+                return tuple(avg.tolist() + [cov, jac])
             return avg, cov, jac
         return wrapped_function
     return wrapper

uncertainty_wrapper/tests/__init__.py
@@ -110,7 +110,13 @@ def test_IV():
     return f(X, VD, __covariance__=COV)
 
 
-@unc_wrapper_args(0,1,2,3,'seconds')
+import pint
+ureg = pint.UnitRegistry()
+
+
+@ureg.wraps(('deg', 'deg', 'dimensionless', 'dimensionless', None, None),
+            ('deg', 'deg', 'millibar', 'degC', None, 'second'))
+@unc_wrapper_args(0, 1, 2, 3, 5)
 def solar_position(lat, lon, press, tamb, timestamps, seconds=0):
     """
     calculate solar position
@@ -145,8 +151,13 @@ def test_solpos():
     Test solar position calculation using NREL's SOLPOS.
     """
     dt = PST.localize(datetime(2016, 4, 13, 12, 30, 0))
-    return solar_position(37.405, -121.95, 1013.25, 20.0, dt, seconds=1,
-                          __covariance__=np.diag([0.0001] * 5))
+    lat = 37.405 * ureg.deg
+    lon = -121.95 * ureg.deg
+    press = 101325 * ureg.Pa
+    tamb = 293.15 * ureg.degK
+    seconds = 1 * ureg.s
+    cov = np.diag([0.0001] * 5)
+    return solar_position(lat, lon, press, tamb, dt, seconds, __covariance__=cov)
 
 if __name__ == '__main__':
     test_unc_wrapper()