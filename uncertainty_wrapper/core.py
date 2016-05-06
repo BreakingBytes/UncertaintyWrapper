fixes #5 fails if covariance keys have multiple args
* also fix bad covariance scaling if more than just diagonal elements
* remove XXX warnings about in place operations on borrowed references hopefully I will remember
* take 2nd dimension of dx from the length of x[n], if x is not rectangular then this will raise an exception
* if cov has 3 dimensions then it means that there are multiple dimensions, so loop over them scaling the covariance by the vector of x in both dimensions to square the cross terms as well as diagonals
* otherwise it means that there may be more than one observation but not in the covariance keys, so just scale by x * x.T again to get cross terms and diagonals
* now can group logic together and tile cov after scaling if necessary for covariance keys with only one item each, but output has multiple observations
* fix tests, oops, f uses different wrappers in each tests, probably more efficient way to write this but works for now

uncertainty_wrapper/core.py
@@ -56,10 +56,8 @@ def partial_derivative(f, x, n, nargs, delta=DELTA):
     :param delta: optional step size, default is :math:`\\epsilon^{1/3}` where
         :math:`\\epsilon` is machine precision
     """
-    dx = np.zeros((nargs, 1))
+    dx = np.zeros((nargs, len(x[n])))
     # scale delta by (|x| + 1.0) to avoid noise from machine precision
-    # XXX: in-place addition changes values of reference to dx, OK since only
-    # used in this scope!
     dx[n] += np.where(x[n], x[n] * delta, delta)
     # apply central difference approximation
     try:
@@ -252,13 +250,13 @@ def unc_wrapper_args(*covariance_keys):
             jac = jacobian(f_, x, nf, nobs, *args, **kwargs)
             # calculate covariance
             if cov is not None:
-                # XXX: do not use in-place multiplication here if reference is
-                # from outside scope!
-                # XXX: use numpy.multiply to copy cov creating a new reference
-                # in this scope 
-                cov = cov * x.T.flatten() ** 2  # scale covariances by x squared
                 # covariance must account for all observations
-                if cov.ndim != 3:
+                # scale covariances by x squared in each direction
+                if cov.ndim == 3:
+                    cov = np.array([c * y * np.row_stack(y)
+                                    for c, y in zip(cov, x.T)])
+                else: # x are all only one dimension
+                    cov = cov * x * x.T
                     assert jac.size / nf / nobs == cov.size / len(x)
                     cov = np.tile(cov, (nobs, 1, 1))
                 # propagate uncertainty using different methods

uncertainty_wrapper/tests/test_uncertainty_wrapper.py
@@ -12,17 +12,17 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
 
-@unc_wrapper
-def func(y):
-    return np.exp(y)
-
-
 def test_unc_wrapper():
     """
     Test uncertainty wrapper.
     """
     x, cov = np.array([[1.0]]), np.array([[0.1]])
-    avg, var, jac = func(x, __covariance__=cov, __method__='dense')
+    
+    @unc_wrapper
+    def f(y):
+        return np.exp(y)
+    
+    avg, var, jac = f(x, __covariance__=cov, __method__='dense')
     LOGGER.debug("average = %g", avg)
     LOGGER.debug("variance = %g", var)
     ok_(np.isclose(avg, np.exp(x)))
@@ -35,8 +35,13 @@ def test_unc_wrapper_args():
     """
     Test uncertainty wrapper.
     """
-    x, cov = np.array(1.0), np.array([[0.1]])    
-    avg, var, jac = func(x, __covariance__=cov, __method__='dense')
+    x, cov = np.array(1.0), np.array([[0.1]])
+    
+    @unc_wrapper_args(None)
+    def f(y):
+        return np.exp(y)
+    
+    avg, var, jac = f(x, __covariance__=cov, __method__='dense')
     LOGGER.debug("average = %g", avg)
     LOGGER.debug("variance = %g", var)
     ok_(np.isclose(avg, np.exp(x)))
@@ -49,8 +54,13 @@ def test_multiple_observations():
     """
     Test uncertainty wrapper.
     """
-    x, cov = np.array([[1.0, 1.0]]), np.array([[[0.1]], [[0.1]]])    
-    avg, var, jac = func(x, __covariance__=cov, __method__='dense')
+    x, cov = np.array([1.0, 1.0]), np.array([[[0.1]], [[0.1]]])  
+    
+    @unc_wrapper_args(None)
+    def f(y):
+        return np.exp(y).reshape(1, -1)
+  
+    avg, var, jac = f(x, __covariance__=cov, __method__='dense')
     LOGGER.debug("average = %g", avg)
     LOGGER.debug("variance = %g", var)
     ok_(np.allclose(avg, np.exp(x)))