b2431588fb6c1cf6f2a54e2afc9bfa8e10067bd0
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2017-08-18 15:22:19 -0700

    wrap uncertianty_wrapper import in try/except

--- uncertainty_wrapper/__init__.py
+++ uncertainty_wrapper/__init__.py
@@ -1,10 +1,14 @@
 """UncertaintyWrapper"""
 
-from uncertainty_wrapper.core import unc_wrapper, unc_wrapper_args, jflatten
-
-__VERSION__ = '0.4.2a'
-__RELEASE__ = u"Cambrian Period"
+__VERSION__ = '0.4.2a1'
+__RELEASE__ = u"Terreneuvian Series"
 __URL__ = u'https://github.com/SunPower/UncertaintyWrapper'
 __AUTHOR__ = u"Mark Mikofski"
 __EMAIL__ = u'mark.mikofski@sunpowercorp.com'
-__all__ = ['unc_wrapper', 'unc_wrapper_args', 'jflatten']
+
+try:
+    from uncertainty_wrapper.core import unc_wrapper, unc_wrapper_args, jflatten
+except ImportError:
+    pass
+else:
+    __all__ = ['unc_wrapper', 'unc_wrapper_args', 'jflatten']
