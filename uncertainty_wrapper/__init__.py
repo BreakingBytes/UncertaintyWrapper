caf1458e79c094ede6a167a54adfb46ca1a7f0b6
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2017-08-18 11:30:18 -0700

    bump version

--- uncertainty_wrapper/__init__.py
+++ uncertainty_wrapper/__init__.py
@@ -2,8 +2,8 @@
 
 from uncertainty_wrapper.core import unc_wrapper, unc_wrapper_args, jflatten
 
-__VERSION__ = '0.4.1'
-__RELEASE__ = u"Paleozoic Era"
+__VERSION__ = '0.4.2a'
+__RELEASE__ = u"Cambrian Period"
 __URL__ = u'https://github.com/SunPower/UncertaintyWrapper'
 __AUTHOR__ = u"Mark Mikofski"
 __EMAIL__ = u'mark.mikofski@sunpowercorp.com'
